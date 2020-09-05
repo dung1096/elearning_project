fc887d361c46a4477e0b6e8a1a6cbc6a5eb710c1
nguyen quang dung <nqdung1096@gmail.com>
2020-09-05 17:10:05 +0700

    d2_buildDetailPage_connectAxios-layChiTietKhoaHoc

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import { BrowserRouter } from "react-router-dom";
 import { HomeTemplate } from "./templates/HomeTemplate/HomeTemplate";
-// import Detail from "./pages/Detail/Detail";
+import Detail from "./pages/Detail/Detail";
 // import SignUp from "./pages/SignUp/SignUp";
 // import Login from "./pages/Login/Login";
 // import Profile from "./pages/Profile/Profile";
@@ -13,8 +13,8 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <HomeTemplate exact path="/home" Component={Home} />
-        {/* <HomeTemplate exact path="/detail/:maKhoaHoc" Component={Detail} />
-        <HomeTemplate exact path="/signup" Component={SignUp} />
+        <HomeTemplate exact path="/detail/:maKhoaHoc" Component={Detail} />
+        {/* <HomeTemplate exact path="/signup" Component={SignUp} />
         <HomeTemplate exact path="/login" Component={Login} />
         <HomeTemplate exact path="/profile" Component={Profile} /> */}
         <HomeTemplate exact path="/" Component={Home} />
