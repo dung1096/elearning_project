6326185490961e9949260434d1ae52065c5d69ed
nguyen quang dung <nqdung1096@gmail.com>
2020-09-08 17:00:19 +0700

    d3_buildLoginPage-SignUpPage

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginContent from "../../components/layout/LoginContent/LoginContent";
+
+export default function Login() {
+  return (
+    <div>
+      <LoginContent />
+    </div>
+  );
+}
---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SignUpContent from "../../components/layout/SignUpContent/SignUpContent";
+
+export default function SignUp() {
+  return (
+    <div>
+      <SignUpContent />
+    </div>
+  );
+}
